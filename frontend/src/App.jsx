11d587a47b51790367569e59ba05e94b32035e74
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-07-10 11:20:16 +0500

    NAVBAR AND FOOTER COMPONENTS IMPORTED AND USED IN APP.JSX FILE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// IMPORTING COMPONENTS
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<h1 className="text-3xl font-bold">Hello World</h1>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
