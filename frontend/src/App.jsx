7bca1ed76e78803d5cff95872b61c2a202c9b5e2
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-07-10 11:14:35 +0500

    imported rect router from 'react-router-dom' into App.jsx file and used it to set up a basic routing structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Hello World</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1 className="text-3xl font-bold">Hello World</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
