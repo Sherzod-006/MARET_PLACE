2a5c29c14f5a77e3100c23fd7e97adcf18631ec9
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-07-10 14:49:38 +0500

    created footer component with social media icons, links and styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,9 +2,55 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <p className="text-center">© 2023 Lion's Shop. All rights reserved.</p>
-    </div>
+    <footer className="bg-primary text-white py-4 mt-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between">
+        <div className='flex flex-col items-center'>
+          <h1 className='text-2xl font-semibold'>Our Supporters</h1>
+          <div className='flex space-x-4 mt-2'>
+            <a href="https://www.alibaba.com" target="_blank" rel="noopener noreferrer" className=''>
+              <h1>Alibaba.com</h1>
+            </a>
+            <a href="https://www.uzum.com" target="_blank" rel="noopener noreferrer" className="">
+              <h1>Uzum Market</h1>
+            </a>
+            <a href="https://www.aliexpress.com" target="_blank" rel="noopener noreferrer" className="">
+              Ali Express
+            </a>
+          </div>
+        </div>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-2xl font-semibold'>Contact Details</h1>
+          <div className='flex space-x-4 mt-2'>
+            <a href="https://t.me/lion_o06" target="_blank" rel="noopener noreferrer" className=''>
+              <h1>Telegram</h1>
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="">
+              <h1>Instagram</h1>
+            </a>
+            <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer" className="">
+                <h1>WhatsApp</h1>
+            </a>
+          </div>
+        </div>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-2xl font-semibold'>Our Socials</h1>
+          <div className='flex space-x-4 mt-2'>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className=''>
+              <img src="./src/assets/icons8-facebook-48.png" alt="" />
+            </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="">
+              <img src="./src/assets/icons8-twitter-48.png" className='' alt="" />
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="">
+              <img src="./src/assets/icons8-instagram-48.png" alt="" />
+            </a>
+          </div>
+        </div>
+        </div>
+        <p className="text-center">© 2023 Lion's Shop. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
