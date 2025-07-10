724b6395f114298871310bf431278cf8f2b6199d
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-07-10 17:13:46 +0500

    imported all the created pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,22 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+// IMPORTING PAGES
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1 className="text-3xl font-bold">Hello World</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </BrowserRouter>
