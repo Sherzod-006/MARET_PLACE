46ed370ead6c59c1b0afa5c82801de5ce1810fe7
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-07-12 21:23:59 +0500

    installed some images for background, and created register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-      <h1>This is the Register Page</h1>
-    </div>
+    <main className="lg:bg-[url(/src/assets/leaves-orange-background-with-copy-space.jpg)] sm:bg-[url(/src/assets/copy-space-yellow-blue-cupboards.jpg)]
+    bg-[url(/src/assets/abstract-luxury-gold-yellow-gradient-studio-wall-well-use-as-backgroundlayoutbanner-product-pres.jpg)] bg-cover bg-center h-screen">
+      <div className="container mx-auto py-2 flex justify-center sm:justify-start md:justify-end items-center p-2">
+        <div className="flex items-center">
+          <img src="./src/assets/Logo.png" alt="logo" className="md:h-30 h-15 pr-5" />
+          <h1 className="text-blue-600 md:text-7xl sm:text-5xl text-3xl font-bold"><span class=" bg-gradient-to-r from-blue-800 via-red-600 to-amber-500 sm:text-transparent bg-clip-text">Lion's</span> <span className="bg-gradient-to-r from-amber-500 via-red-600 to-blue-600 sm:text-transparent bg-clip-text">Company</span>
+          </h1>
+        </div>
+      </div>   
+    </main>
   )
 }
 
